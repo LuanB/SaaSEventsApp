d3734af53f0cd313b13eefdcdf9e72f7394356c3
Luan <luan.b@stjamesperth.com.au>
2019-02-06 13:20:49 +0800

    required in stripe and pass in keys, installed bodyparser middleware to parse get data on the server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 
@@ -11,6 +12,11 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// middlewares
+
+//bodyParser will parse any get into the server an parse the data for use.
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
