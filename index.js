e66a7583efc3e0b0db018c3ce33ed2fdf58e12bb
Luan <luan.b@stjamesperth.com.au>
2019-02-07 10:52:00 +0800

    setup package.json to install dependencies on heroku postbuild

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // express will server up production assets
+  // like main.js file or main.css file
+  // if express sees any route that is not defined then go and check clinet/build/static/main to see if any files match
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file (ie react router: /surveys) if it doen't recognize the route after checking
+  //  /rotes/authRoutes , billingRoutes & client/build
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // or require('./routes/authRoutes')(app) to bring in the routes for the app.
 
 // dynamic port bindig for heroku
